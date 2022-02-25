251e9048b9bc56b0ed5436c45859f28b46b55b30
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-25 22:48:23 +0100

    Create GhostHunters.vue

---
+++ src/components/GhostHunters.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h2>Hunters</h2>
+    <ul>
+      <li v-for="hunter in hunters" :key="hunter.id">{{ hunter.name }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hunters: this.$store.getters.hunters,
+    };
+  },
+};
+</script>
+
+<style></style>
