d230e3879c62165eabfda6b259f7c5e64b2ae1de
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-25 22:48:26 +0100

    Create UserInterface.vue

---
+++ src/components/UserInterface.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ui-wrapper">
+    <div class="ui">
+      <p class="ui__cash"><b>Cash:</b> ${{ user.cash }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: this.$store.getters.user,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.ui-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border-bottom: 2px solid #222;
+}
+
+.ui {
+  width: $desktop-screen-width;
+  margin: auto;
+  padding: 2rem;
+  height: 6rem;
+  &__cash {
+  }
+}
+</style>
