264e8376829939a5877c374dc79db6f66b96f8ff
Daniel Gæbe Svendsen <danielsv@trophy-games.com>
2022-04-03 11:25:24 +0200

    Added BootstrapVue
    Added BootstrapVue and updated dependencies so the project works with no errors and upgradable into Bootstrap5

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 import store from './store'
 
@@ -8,3 +9,16 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// BOOTSTRAP VUE //
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+// SCSS //
+import './App.scss' // Default overrides
+Vue.use(BootstrapVue) // Telling Vue to use this in whole application
+
+// Basic Icons
+Vue.use(IconsPlugin)
