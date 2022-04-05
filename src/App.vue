04573376f43587e53fb267553644809e3f1f6d56
Daniel Gæbe Svendsen <danielsv@trophy-games.com>
2022-04-05 11:00:17 +0200

    Build testing

--- src/App.vue
+++ src/App.vue
@@ -54,4 +54,11 @@ export default {
 </script>
 
 <style lang="scss">
+// Import custom SASS variable overrides, or alternatively
+// define your variable overrides here instead
+@import "assets/style/variables.scss";
+
+// Import Bootstrap and BootstrapVue source SCSS files
+@import "~bootstrap/scss/bootstrap.scss";
+@import "~bootstrap-vue/src/index.scss";
 </style>
